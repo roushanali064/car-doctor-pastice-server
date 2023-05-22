119afc82fa6ec8337d3aca8853274833b5ffa31e
roushanali064 <roushanrhaman064@gmail.com>
2023-05-22 18:15:10 +0600

    search and sort done

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ const client = new MongoClient(uri, {
 });
 
 const verify = (req,res,next)=>{
-    console.log('hating jwt');
-    console.log(req.headers.authorization)
+    // console.log('hating jwt');
+    // console.log(req.headers.authorization)
     const authorization = req.headers.authorization;
     if(!authorization){
         return res.status(401).send({error: true, message: 'unauthorized access'})
     }
     const token = authorization.split(' ')[1];
-    console.log('token inside', token)
+    // console.log('token inside', token)
     jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (error,decoded)=>{
         if(error){
             res.send({error: true, message: 'unauthorized access'})
@@ -47,14 +47,14 @@ const verify = (req,res,next)=>{
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const serviceCollection = client.db('carDoctor').collection('services')
         const checkOutCollection = client.db('carDoctor').collection('checkOuts')
         //jwt
         app.post('/jwt',(req,res)=>{
             const user = req.body;
-            console.log(user);
+            // console.log(user);
             const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
                 expiresIn: '1hr'
             })
@@ -63,9 +63,25 @@ async function run() {
 
         //service routes
         app.get('/services', async (req, res) => {
-            const cursor = serviceCollection.find();
-            const result = await cursor.toArray();
-            res.send(result)
+            const search = req.query.search
+            const query = {
+                price: {$lt: 300, $gt: 0},
+                title: { $regex: search , $options: 'i'}
+            };
+            // 
+            const sort = req.query.sort
+            
+            if (sort == "asc") {
+                // console.log('asc')
+                const cursor = serviceCollection.find(query).sort({ price: 1 });
+                const result = await cursor.toArray();
+                res.send(result);
+              } else {
+                // console.log('dsc')
+                const cursor = serviceCollection.find(query).sort({ price: -1 });
+                const result = await cursor.toArray();
+                res.send(result);
+              }
         })
 
         app.get('/services/:id', async (req, res) => {
@@ -91,7 +107,7 @@ async function run() {
         app.get('/checkOuts', verify, async (req, res) => {
             // console.log(req.headers.authorization)
             const decoded = req.decoded;
-            console.log(decoded)
+            // console.log(decoded)
             if(decoded.email !== req.query.email){
                 return req.status(403).send({error: 1, message: 'forbidden access'})
             }
@@ -108,7 +124,7 @@ async function run() {
 
         app.post('/checkOuts', async (req, res) => {
             const checkOut = req.body;
-            console.log(checkOut)
+            // console.log(checkOut)
             const result = await checkOutCollection.insertOne(checkOut)
             res.send(result)
         })
